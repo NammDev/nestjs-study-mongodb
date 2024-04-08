9b586689b30645391169e793b3735a38590fe035
Nam <namdeveloper.ca@gmail.com>
2024-04-08 15:08:19 +0700

    lesson type define

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
@@ -7,6 +8,7 @@ import { GraphQLModule } from '@nestjs/graphql';
       // save the schema in a memory & regenerate it on each start
       autoSchemaFile: true,
     }),
+    LessonModule,
   ],
   controllers: [],
   providers: [],
