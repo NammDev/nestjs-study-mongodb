cf1b9be0fabe1b316035394752088066bb4a8333
Nam <namdeveloper.ca@gmail.com>
2024-04-08 15:47:23 +0700

    create lesson entity & typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb+srv://namdeveloperca:WpG7TTVCGsvFzx7j@school-management.dph4dta.mongodb.net/?retryWrites=true&w=majority',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // save the schema in a memory & regenerate it on each start
