765a71b80c0a98e6d0df91e97804dc10d1ce203f
Nam <namdeveloper.ca@gmail.com>
2024-04-08 15:19:34 +0700

    config graphql & apollo running

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       // save the schema in a memory & regenerate it on each start
       autoSchemaFile: true,
     }),
